4e22f04c02f88e48660ef3d2098a8a87efcd1556
Verena <Verena.kipp@student.hs-rm.de>
2025-04-26 11:02:02 +0200

    first commit

---
+++ src/main/java/de/hsrm/mi/web/derdigitaledoenerverleih/DerdigitaledoenerverleihApplication.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.web.derdigitaledoenerverleih;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DerdigitaledoenerverleihApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DerdigitaledoenerverleihApplication.class, args);
+	}
+
+}
---
+++ src/test/java/de/hsrm/mi/web/derdigitaledoenerverleih/DerdigitaledoenerverleihApplicationTests.java
@@ -0,0 +1,13 @@
+package de.hsrm.mi.web.derdigitaledoenerverleih;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DerdigitaledoenerverleihApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
