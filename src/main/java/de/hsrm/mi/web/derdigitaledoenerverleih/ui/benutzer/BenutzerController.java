cc61f774df7b763f4ca7e2ade479e57db5c39c6a
Verena <Verena.kipp@student.hs-rm.de>
2025-04-26 16:08:57 +0200

    FEAT: BenutzerFormulate als Map in Session gespeichert

--- src/main/java/de/hsrm/mi/web/derdigitaledoenerverleih/ui/benutzer/BenutzerController.java
+++ src/main/java/de/hsrm/mi/web/derdigitaledoenerverleih/ui/benutzer/BenutzerController.java
@@ -1,23 +1,38 @@
 package de.hsrm.mi.web.derdigitaledoenerverleih.ui.benutzer;
 
+import java.util.HashMap;
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 
 
 
 @Controller
+@SessionAttributes(names = {"benutzerMap"})
 public class BenutzerController {
 
     @GetMapping("/benutzer/{loginName}")
     public String getBenutzer(@PathVariable("loginName") String name,
                                 @ModelAttribute("formular") BenutzerFormular benutzerFormular,
                                 Model model) {
-        
+        Map<String, BenutzerFormular> map;
+        map = (Map<String, BenutzerFormular>) model.getAttribute("benutzerMap");
+        if(map == null){
+            map = new HashMap<>();
+            model.addAttribute("name", name);
+            return "benutzer/bearbeiten.html";
+        }
+        if(map.containsKey(name)){
+            benutzerFormular = map.get(name);
+            model.addAttribute("formular", benutzerFormular);
+        }
         model.addAttribute("name", name);
         return "benutzer/bearbeiten.html";
     }
@@ -29,7 +44,18 @@ public class BenutzerController {
                                     ) {
 
         model.addAttribute("name", benutzerFormular.getName());
-        model.addAttribute("fomrular", benutzerFormular);
+        model.addAttribute("formular", benutzerFormular);
+        Map<String, BenutzerFormular> map;
+        // @SuppressWarnings("unckecked")
+        map = (Map<String, BenutzerFormular>) model.getAttribute("benutzerMap"); //Model gibt immer ein Objekt zurück
+
+        if(map == null){
+            map = new HashMap<String, BenutzerFormular>();
+        }
+
+        map.put(benutzerFormular.getName(), benutzerFormular);
+
+        model.addAttribute("benutzerMap", map);
         
         return "benutzer/bearbeiten.html";
     }
